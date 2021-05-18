8fc7ef396b5aed4e1cdc43aef24e289a3c1817f4
Lobanov Pavel <lobanov.ps@gmail.com>
2021-05-18 16:51:45 +0300

    Deleted version printing

--- cmd/ocp-project-api/main.go
+++ cmd/ocp-project-api/main.go
@@ -1,47 +1,7 @@
 package main
 
-import (
-		"fmt"
-		"io"
-		"os/exec"
-		"strings"
-	)
-
-func runCommandWithInput(cmdName string, args string, input string) string {
-	cmd := exec.Command(cmdName, args)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return ""
-	}
-
-	go func() {
-		defer stdin.Close()
-		io.WriteString(stdin, input)
-	}()
-
-	stdout, err := cmd.CombinedOutput()
-	if err != nil {
-		return ""
-	}
-
-	return string(stdout)
-}
-
-func getCurrentVersion() string {
-	cmd := exec.Command("git", "tag", "--sort=committerdate")
-	stdout, err := cmd.Output()
-
-    if err != nil {
-        return ""
-    }
-
-    // Print the output
-    tag := runCommandWithInput("tail", "-n 1", string(stdout))
-    return strings.Trim(tag, "\n")
-}
+import "fmt"
 
 func main() {
     fmt.Println("Hello, I'm ocp-project-api service!")
-
-	fmt.Println("My version: ", getCurrentVersion())
 }
