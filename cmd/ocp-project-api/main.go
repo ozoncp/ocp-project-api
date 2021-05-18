7cdc5a5744a3f48e8df5e61442edfe4ce923f6a0
Lobanov Pavel <lobanov.ps@gmail.com>
2021-05-18 16:30:40 +0300

    Added version printing

--- cmd/ocp-project-api/main.go
+++ cmd/ocp-project-api/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"io"
+		"os/exec"
+	)
+
+func getCurrentVersion() string {
+	cmd := exec.Command("git", "tag", "--sort=committerdate")
+	stdout, err := cmd.Output()
+
+    if err != nil {
+        return ""
+    }
+
+    // Print the output
+    tags := string(stdout)
+
+	cmd = exec.Command("tr", "-n 1")
+    stdin, err := cmd.StdinPipe()
+    if err != nil {
+        return ""
+    }
+
+    go func() {
+        defer stdin.Close()
+        io.WriteString(stdin, tags)
+    }()
+
+    stdout, err = cmd.CombinedOutput()
+    if err != nil {
+        return ""
+    }
+
+    return string(stdout)
+}
 
 func main() {
     fmt.Println("Hello, I'm ocp-project-api service!")
+
+	fmt.Println("My version: ", getCurrentVersion())
 }
